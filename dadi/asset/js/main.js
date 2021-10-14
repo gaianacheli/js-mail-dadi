6f5747e58bdddbadd8a071ed2ff41613bcbde42c
gaianacheli <gaianacheli04@gmail.com>
2021-10-14 19:20:27 +0200

    struttura html con inserimento tag e collegati oggetti DOM

---
+++ dadi/asset/js/main.js
@@ -0,0 +1,12 @@
+/*Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+//collego oggetti DOM
+const btnPlay = document.getElementById('btnplay')
+const giocatore = document.getElementById('giocatore')
+const vincitore = document.getElementById('vincitore')
+const computer = document.getElementById('computer')
+
+btnPlay.addEventListener('click',function(){
+    let numeroComputer = 
+})
